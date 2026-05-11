86739a11f1026630ff29758a40f7bc0f72abd9e7
nexus <82994528+Staticnoi@users.noreply.github.com>
2026-05-11 21:18:47 +0700

    Switched Prisma to PostgreSQL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/HR-Management-System/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
